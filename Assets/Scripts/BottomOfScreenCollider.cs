dadd271eb69d0f76efb0013d6889f51d94a71cfb
rbibler <rbibler01@hotmail.com>
2015-12-08 22:30:01 -0500

    First Commit

---
+++ Assets/Scripts/BottomOfScreenCollider.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottomOfScreenCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		levelManager.LoadLevel ("Win Screen");
+	}
+}
